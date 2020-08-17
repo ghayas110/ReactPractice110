1baa840fdae69f8a0303f8778c812efcb9916625
Ghayas <ghayas110@gmail.com>
2020-08-17 21:44:10 +0500

    Ghayas

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [person,setPerson]=useState({name: 'Ghayas', enrol: "02235172003", reg: 51225});
+const clickHandler=() =>{
+  setPerson({name:"Hamza Zafar",enrol: "02-235-172-004", reg: 51226});
+}
   return (
+   
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}> React Developer</Text>
+<View style={styles.body}>
+  <Text>Name:{person.name}</Text>
+  <Text>Enrol:{person.enrol}</Text>
+  <Text>RegNo:{person.reg}</Text>
+</View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Show More" onPress={clickHandler}/>
+      </View>
+     
     </View>
   );
 }
@@ -17,5 +31,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header:{
+    backgroundColor: 'yellow',
+    padding:20,
+  },
+  boldText:{
+fontWeight: "bold",
+textAlign:"center"
+
+  },
+  body:{
+backgroundColor:"lightgreen",
+padding:20
+  },
+  buttonContainer:{
+    color:"black"
+
   },
 });
